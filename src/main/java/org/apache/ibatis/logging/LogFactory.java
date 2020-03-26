c5ca2686798e516675c7ba6bddfc85f652c88c7a
suxiongwei <suxiongwei@kaoshixing.com>
2020-03-26 20:55:54 +0800

    docs: 初始化工程项目

---
+++ src/main/java/org/apache/ibatis/logging/LogFactory.java
@@ -0,0 +1,124 @@
+/**
+ *    Copyright ${license.git.copyrightYears} the original author or authors.
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+package org.apache.ibatis.logging;
+
+import java.lang.reflect.Constructor;
+
+/**
+ * @author Clinton Begin
+ * @author Eduardo Macarron
+ * learn
+ */
+public final class LogFactory {
+
+  /**
+   * Marker to be used by logging implementations that support markers
+   */
+  public static final String MARKER = "MYBATIS";
+
+  /**
+   * 被选定的第三方日志组件适配器的构造方法
+   */
+  private static Constructor<? extends Log> logConstructor;
+
+  /**
+   * 自动扫描日志实现，并且第三方日志插件优先级如下：slf4j -> commonsLogging -> log4j2 -> log4j -> jdkLog
+   */
+  static {
+    tryImplementation(LogFactory::useSlf4jLogging);
+    tryImplementation(LogFactory::useCommonsLogging);
+    tryImplementation(LogFactory::useLog4J2Logging);
+    tryImplementation(LogFactory::useLog4JLogging);
+    tryImplementation(LogFactory::useJdkLogging);
+    tryImplementation(LogFactory::useNoLogging);
+  }
+
+  private LogFactory() {
+    // disable construction
+  }
+
+  public static Log getLog(Class<?> aClass) {
+    return getLog(aClass.getName());
+  }
+
+  public static Log getLog(String logger) {
+    try {
+      return logConstructor.newInstance(logger);
+    } catch (Throwable t) {
+      throw new LogException("Error creating logger for logger " + logger + ".  Cause: " + t, t);
+    }
+  }
+
+  public static synchronized void useCustomLogging(Class<? extends Log> clazz) {
+    setImplementation(clazz);
+  }
+
+  public static synchronized void useSlf4jLogging() {
+    setImplementation(org.apache.ibatis.logging.slf4j.Slf4jImpl.class);
+  }
+
+  public static synchronized void useCommonsLogging() {
+    setImplementation(org.apache.ibatis.logging.commons.JakartaCommonsLoggingImpl.class);
+  }
+
+  public static synchronized void useLog4JLogging() {
+    setImplementation(org.apache.ibatis.logging.log4j.Log4jImpl.class);
+  }
+
+  public static synchronized void useLog4J2Logging() {
+    setImplementation(org.apache.ibatis.logging.log4j2.Log4j2Impl.class);
+  }
+
+  public static synchronized void useJdkLogging() {
+    setImplementation(org.apache.ibatis.logging.jdk14.Jdk14LoggingImpl.class);
+  }
+
+  public static synchronized void useStdOutLogging() {
+    setImplementation(org.apache.ibatis.logging.stdout.StdOutImpl.class);
+  }
+
+  public static synchronized void useNoLogging() {
+    setImplementation(org.apache.ibatis.logging.nologging.NoLoggingImpl.class);
+  }
+
+  private static void tryImplementation(Runnable runnable) {
+    /**
+     * logConstructor为空才执行，当已经匹配到日志实现时，后面的就不会继续执行
+     */
+    if (logConstructor == null) {
+      try {
+        runnable.run();
+      } catch (Throwable t) {
+        // ignore
+      }
+    }
+  }
+
+  private static void setImplementation(Class<? extends Log> implClass) {
+    try {
+      Constructor<? extends Log> candidate = implClass.getConstructor(String.class);
+      Log log = candidate.newInstance(LogFactory.class.getName());
+      if (log.isDebugEnabled()) {
+        log.debug("Logging initialized using '" + implClass + "' adapter.");
+      }
+      // learn：设置logConstructor,一旦设上，表明找到相应的log的jar包了，那后面别的log就不找了。
+      logConstructor = candidate;
+    } catch (Throwable t) {
+      throw new LogException("Error setting Log implementation.  Cause: " + t, t);
+    }
+  }
+
+}
